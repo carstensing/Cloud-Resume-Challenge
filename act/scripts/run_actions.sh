0dd682e750719507631a818eeee9a2bd2f45a559
Carsten Singleton <carstensing@gmail.com>
2025-04-02 15:32:31 -0700

    Updated run_terraform workflow.

--- act/scripts/run_actions.sh
+++ act/scripts/run_actions.sh
@@ -7,9 +7,8 @@ cd "${git_root}"
 act push -q \
 --action-offline-mode \
 -P ubuntu-latest=my-act-aws-image \
+-W "${git_root}/.github/workflows/run_terraform.yaml" \
 --secret-file "${git_root}/act/inputs/.secrets" \
 --env-file "${git_root}/act/inputs/.env" \
 --var-file "${git_root}/act/inputs/.vars" \
---artifact-server-path "${git_root}/act/inputs/artifacts"
-
-# -W "${git_root}/.github/workflows/lambda_test.yaml" \
+--artifact-server-path "${git_root}/act/artifacts"
