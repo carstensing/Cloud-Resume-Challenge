447eba1e7c326b6efe323066ddb3262d5539b356
Carsten Singleton <carstensing@users.noreply.github.com>
2025-04-04 11:37:49 -0700

    Generated files during GitHub Action.

--- backend/src/terraform/main.tf
+++ backend/src/terraform/main.tf
@@ -419,7 +419,7 @@ data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = local.lambda_src_path
   output_path = local.lambda_zip_path
-  excludes    = ["${local.lambda_zip_path}", "__pycache__"]
+  excludes    = ["lambda.zip", "__pycache__"]
 }
 
 # Create a lambda function
